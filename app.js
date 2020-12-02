6fd6e5ebfdf88dcaec6bfb7e5ea3b62474761960
ShyroVO <Bosquet.laurie@outlook.com>
2020-12-02 11:33:17 +0100

    1.0

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-function maFonction(p1, param2) {
+function maFonction (id, texte) {
 
-    document.getElementById("p1").innerText=param2;
-    document.getElementById("p2").innerText=param21;
-    document.getElementById("p3").innerText=param22;
-    document.getElementById("p4").innerText=param23;
-    document.getElementById("p5").innerText=param24;
+    let paragraphe = document.getElementById(id);
+    paragraphe.innerText=texte;
 
 }
 
-let param21 = "Text a mettre en place 2";
-let param22 = "Text a mettre en place 3";
-let param23 = "Text a mettre en place 4";
-let param24 = "Text a mettre en place 5";
-
-maFonction("","Text a mettre en place!");
+maFonction("p1","Text a mettre en place!");
+maFonction("p2","Text a mettre en place 2!");
+maFonction("p3","Text a mettre en place 3!");
+maFonction("p4","Text a mettre en place 4!");
+maFonction("p5","Text a mettre en place 5!");
