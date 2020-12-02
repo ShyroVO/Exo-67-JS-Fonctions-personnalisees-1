4a16a2439effefe69ddd250788aa2e2629fdc930
ShyroVO <Bosquet.laurie@outlook.com>
2020-12-02 11:18:20 +0100

    0.5

---
+++ app.js
@@ -0,0 +1,16 @@
+function maFonction(p1, param2) {
+
+    document.getElementById("p1").innerText=param2;
+    document.getElementById("p2").innerText=param21;
+    document.getElementById("p3").innerText=param22;
+    document.getElementById("p4").innerText=param23;
+    document.getElementById("p5").innerText=param24;
+
+}
+
+let param21 = "Text a mettre en place 2";
+let param22 = "Text a mettre en place 3";
+let param23 = "Text a mettre en place 4";
+let param24 = "Text a mettre en place 5";
+
+maFonction("","Text a mettre en place!");
